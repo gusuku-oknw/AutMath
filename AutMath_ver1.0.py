bc94629ac9a98901a38386590daae68c2c77f723
jun <tmkjny@gmail.com>
2022-12-18 23:23:07 +0900

    desktop

--- AutMath_ver1.0.py
+++ AutMath_ver1.0.py
@@ -158,7 +158,7 @@ class AutMath:
             print("value1:{} value2:{} value3:{}".format(value1, value2, value3))
             val, answer = self.four_rules(value1, value2)
             if ('×' in val) or ('÷' in val):
-                print(self.four_rules(answer, value3, polynomial=val))
+                print(self.four_rules(value3, answer, polynomial=val))
             elif ('+' in val) or ('-' in val):
                 print(self.four_rules(answer, value3, polynomial=val))
 
@@ -186,12 +186,14 @@ class AutMath:
                 r = -1
                 print("不等号がありません")
 
+            val0 = polynomial[:r+1]
+
             if ((rules == 1) or (rules == 2)) and (('×' in polynomial[r]) or ('÷' in polynomial[r])):
+                val1, val2 = val2, val1
                 break
             # elif ((rules == 1) or (rules == 2)) and (('×' in polynomial) or ('÷' in polynomial)):
             #     break
             elif ((rules == 3) or (rules == 4)) and (('+' in polynomial[r]) or ('-' in polynomial[r])):
-                val0 = polynomial[:r+1]
                 val1 = polynomial[r+1:]
                 val1 = val1.replace("(", "").replace(")", "")
                 print('val0:({}),val1:({}), {}'.format(val0, val1, r))
